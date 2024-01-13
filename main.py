07b566feaca4ee436197533acbcbcf51082c3039
brayanbarret0 <brayanbarreto1130@gmail.com>
2024-01-13 18:43:22 -0400

    Adding webapp command

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from decouple import config
 
-from src.commands.BasicCommands import start, menu_actions
+from src.commands.BasicCommands import start, menu_actions, web_app_data
 
 def main():
-    app = Application.builder().token(config("API_KEY")).build()
+    bot = Application.builder().token(config("API_KEY")).build()
 
-    app.add_handler(CommandHandler('start', start))
-    app.add_handler(CallbackQueryHandler(menu_actions))
+    bot.add_handler(CommandHandler('start', start))
+    bot.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
+    bot.add_handler(CallbackQueryHandler(menu_actions))
 
-    app.run_polling()
+    bot.run_polling()
 
 if __name__ == "__main__":
     main()
--- src/commands/BasicCommands.py
+++ src/commands/BasicCommands.py
@@ -1,17 +1,28 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import (
+    Update,
+    InlineKeyboardMarkup,
+    WebAppInfo,
+    KeyboardButton,
+    ReplyKeyboardRemove
+)
 from telegram.ext import ContextTypes
+import json
+
+async def web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    data = json.loads(update.effective_message.web_app_data.data)
+    await update.message.reply_html( text= f"{data}", reply_markup= ReplyKeyboardRemove())
 
 async def start(update: Update, context: ContextTypes):
     username = update.message.chat.to_dict()["first_name"]
     menu_main = [
-        [InlineKeyboardButton('Shop', callback_data='shop')]
+        [KeyboardButton('Shop', web_app=WebAppInfo(url="https://e8ed-186-14-22-30.ngrok-free.app"))]
     ]
 
     msg = f"Hello {username}, Welcome to the bot ecommerce!.\n\nUse the following commands to get started."
     reply_markup = InlineKeyboardMarkup(menu_main)
 
     await update.message.reply_text(msg, reply_markup= reply_markup)
-
+    
 async def menu_actions(update: Update, context: ContextTypes):
     query = update.callback_query
     if query.data == "shop":
