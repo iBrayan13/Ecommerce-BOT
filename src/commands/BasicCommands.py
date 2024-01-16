54bfe5005ab9e07a4ff08711a6e454f15b6f575c
brayanbarret0 <brayanbarreto1130@gmail.com>
2024-01-16 16:13:59 -0400

    Webapp functionality ready

--- src/commands/BasicCommands.py
+++ src/commands/BasicCommands.py
@@ -2,7 +2,8 @@ from telegram import (
     Update,
     InlineKeyboardMarkup,
     WebAppInfo,
-    KeyboardButton
+    KeyboardButton,
+    InlineKeyboardButton,
 )
 from telegram.ext import ContextTypes
 import json
@@ -10,7 +11,33 @@ from decouple import config
 
 async def web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     data = json.loads(update.effective_message.web_app_data.data)
-    print(data)
+    userdata = update.message.chat.to_dict()
+
+    # Verifying order
+    for product in data['order']:
+        if not product['amount'] <= product['stock']:
+            await update.message.reply_text(
+                f"Sorry, {userdata['first_name']}. You try to order {product['amount']} {product['name']}, but our stock is {product['stock']} units."
+            )
+            raise ValueError
+        
+        elif not product['amount'] > 0:
+            await update.message.reply_text(
+                f"Sorry, {userdata['first_name']}. You try to order and invalid amount of {product['name']}. Try again with other value."
+            )
+            raise ValueError
+
+    # Getting order
+    msg = f"Hey {userdata['first_name']}! You have been ordered:\n"
+    total = 0
+    for product in data['order']:
+        by_product = product['price'] * product['amount']
+        total += by_product
+        msg += f"\nProduct: {product['name']}\nPrice: ${product['price']}\nUnits: {product['amount']}\nTotal by this product: ${by_product}\n"
+    msg += f"\nTotal: ${total}"
+    
+    reply_btn = [InlineKeyboardButton('✅ CONFIRM ✅', callback_data='accepted')]
+    await update.message.reply_text(msg, reply_markup=InlineKeyboardMarkup([reply_btn]))
 
 async def start(update: Update, context: ContextTypes):
     userdata = update.message.chat.to_dict()
@@ -30,5 +57,5 @@ async def start(update: Update, context: ContextTypes):
     
 async def menu_actions(update: Update, context: ContextTypes):
     query = update.callback_query
-    if query.data == "shop":
-        await query.message.reply_text("This is the shop")
+    if query.data == "accepted":
+        await query.message.reply_text("ORDER SENT ✅")
